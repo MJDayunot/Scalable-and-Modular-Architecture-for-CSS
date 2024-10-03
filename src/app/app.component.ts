b6196eb553993ab1026942641f43b472da56637d
MJDayunot <mjanedayunot@gmail.com>
2024-10-03 21:02:01 +0800

    AddSMACSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "./components/header/header.component";
+import {AboutComponent} from "./components/about/about.component";
+import {ContactComponent} from "./components/contact/contact.component";
+import {FooterComponent} from "./components/footer/footer.component";
+import {ProjectsComponent} from "./components/projects/projects.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, AboutComponent, ProjectsComponent, ContactComponent, FooterComponent, ProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
